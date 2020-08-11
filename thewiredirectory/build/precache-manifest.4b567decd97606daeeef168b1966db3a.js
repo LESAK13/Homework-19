42eb78f34b92877ad56098a3d10e647e41ebbc95
LESAK13 <lesak13@gmail.com>
2020-08-11 17:49:05 -0400

    testing deployment

---
+++ thewiredirectory/build/precache-manifest.4b567decd97606daeeef168b1966db3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae33f610bb7312e15634e5c76eb758a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e15898a4c381f053b4fd",
+    "url": "/static/css/main.c1f0faec.chunk.css"
+  },
+  {
+    "revision": "5522e5b4473801417c58",
+    "url": "/static/js/2.57fe60a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57fe60a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15898a4c381f053b4fd",
+    "url": "/static/js/main.4caf3996.chunk.js"
+  },
+  {
+    "revision": "26d5b5fc4d28fa5e258e",
+    "url": "/static/js/runtime-main.b1827233.js"
+  }
+]);
